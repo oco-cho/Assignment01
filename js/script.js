e12048678bea467fe48d5d532732d009e2d68c48
oco-cho <77410049+oco-cho@users.noreply.github.com>
2021-02-12 17:27:32 -0800

    init

---
+++ js/script.js
@@ -0,0 +1,165 @@
+/*
+//STEP 1
+let someMonth;
+function theMonth();
+let currentMonth;
+let summerMonth;
+let myLibraryFunction;
+*/
+
+
+/*
+//STEP 2
+2 //numeric
+'This is a string literal' //string
+false //Boolean
+null //null
+*/
+
+
+/*
+//STEP 3
+let toyCollection = function () {
+    let toys = ['cowboy', 'dinosour', 'GI Joe'];
+    alert(`My most favorite toy in my collection is the ${toys[1]}!`);
+}
+
+let cowboys = 2 * 4 / 2;
+*/
+
+
+/*
+//STEP 4
+let strFirstName;
+let strLastName;
+let strAddress;
+let strCity;
+let strState;
+let strZip; //String or Number??
+let intAge;
+let strReferralSource;
+let blnMayWeContactYou;
+*/
+
+
+/*
+//STEP 5
+let strFirstName;
+strFirstName = 'Kristen';
+
+let strLastName = prompt('What is your last name?');
+
+let intAge = [];
+intAge[0] = 10;
+intAge[1] = 24;
+*/
+
+
+/*
+//STEP 6
+let step6 = -400 + 'Hello, I am a string of text.';
+console.log('Well this is embarrassing! My negative integer was converted to a ' + typeof (step6) + ': ' + step6);
+*/
+
+
+/*
+//STEP 7
+let var1 = false + 'Hello, I am a string of text.';
+console.log('Help! My Boolean was converted to a ' + typeof (var1) + ': ' + var1);
+
+let var2 = 1 + true;
+console.log('Oh no! My Boolean was converted to a ' + typeof (var2) + ': ' + var2);
+*/
+
+/*
+//STEP 8 -- answer: not correct. Missing backslash escape character
+let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+window.console.log(someString);
+*/
+
+/*
+//STEP 9
+let step9 = null;
+console.log(step9);
+*/
+
+
+/*
+//STEP 10
+console.log(typeof 'This is a string');
+console.log(typeof 2);
+console.log(typeof false);
+console.log(typeof (x = []));
+console.log(typeof y);
+*/
+
+
+/*
+//STEP 11
+alert('Hello ' + 'Kristen' + ', welcome to the JavaScript class!');
+
+//STEP 12 - JS Code got cranky with me and said name was depricated. Substitued it w/ myName.
+let myName = 'Kristen';
+alert('Hello ' + myName + ', welcome to the JavaScript class!');
+
+//STEP 13
+let course = 'JavaScript';
+alert('Hello ' + myName + ', welcome to the ' + course + ' class!');
+
+//STEP 14
+alert('Hello ' + myName + '.' + '\nWelcome to the ' + course + ' class!');
+
+//STEP 15
+let namePrompt = prompt('What is your first name?');
+alert('Hello ' + namePrompt + '.' + '\nWelcome to the ' + course + ' class!');
+
+//STEP 16
+let coursePrompt = prompt('What course are you currently taking?');
+alert('Hello ' + namePrompt + '.' + '\nWelcome to the ' + coursePrompt + ' class!');
+*/
+
+
+/*
+//STEP 17
+let x = 10;
+let y = 20;
+console.log(x + y);
+*/
+
+
+/*
+//STEP 18
+let x = 20;
+x +=20;
+console.log(x);
+*/
+
+
+/*
+//STEP 19
+let x = 20;
+x *= 5;
+console.log(x);
+*/
+
+
+/*
+//STEP 20
+let x = 20 % 3;
+x /= 1;
+console.log(x); 
+*/
+
+/*
+//STEP 21
+let x = 10;
+let y = 50;
+alert(x < 50 && x !== y);
+*/
+
+/*
+//STEP 22
+let x = 10;
+let y = 50;
+alert(x >= y || x === y);
+*/
